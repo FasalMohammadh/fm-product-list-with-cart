ae1e04a0ce82490da1052d3828f2003e4c6d4dc2
Fasal Mohammadh <fasal@intaap.com>
2024-07-17 12:36:10 +0530

    docs(matchBy): added reason for usage of any

--- src/utils/array-utils.ts
+++ src/utils/array-utils.ts
@@ -2,11 +2,11 @@ function mergeBy<T, U, K extends Extract<keyof T, keyof U>>(
   array1: T[],
   array2: U[],
   key: K
-): (T & U)[] {
+) {
   const newArray: (T & U)[] = [];
 
   for (const item of array1) {
-    const match = array2.find((i) => i[key] === (item[key] as any));
+    const match = array2.find((i) => i[key] === (item[key] as any)); // any added to make typescript stop complaining
     if (typeof match !== "undefined") {
       newArray.push({ ...item, ...match });
     }
